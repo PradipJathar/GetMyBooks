e2a1d73a55cf85becaa40a0b1db4447b067c7ebe
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-06-17 16:10:21 +0530

    Section 2 - Data Annotations

--- GetMyBooks/Models/Category.cs
+++ GetMyBooks/Models/Category.cs
@@ -4,8 +4,10 @@ namespace GetMyBooks.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public int DisplayOrder { get; set; }
