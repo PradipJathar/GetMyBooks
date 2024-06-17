62b4ad245dd596b602bf6da65b023046f981e659
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-06-17 16:58:10 +0530

    Section 2 - Create Category Table

--- GetMyBooks/Data/ApplicationDbContext.cs
+++ GetMyBooks/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GetMyBooks.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GetMyBooks.Data
 {
@@ -7,5 +8,7 @@ namespace GetMyBooks.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {            
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
