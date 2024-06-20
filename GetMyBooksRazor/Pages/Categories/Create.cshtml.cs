dbdef0258d68893311e0d5ec8ea161c5ad563f7d
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-06-20 12:41:35 +0530

    Section 3 - Create Category UI

---
+++ GetMyBooksRazor/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,24 @@
+using GetMyBooksRazor.Data;
+using GetMyBooksRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GetMyBooksRazor.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext db;
+        public Category Category { get; set; }
+
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+
+        public void OnGet()
+        {
+        }
+    }
+}
